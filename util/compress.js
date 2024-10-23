5391df0568135d1297d246e3e78a796f388f4c12
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-10-23 18:08:29 +0700

    Update compress.js

--- util/compress.js
+++ util/compress.js
@@ -1,34 +1,34 @@
 const sharp = require("sharp");
 
-function compress(input, webp, grayscale, quality, originSize) {
-	const format = webp ? "webp" : "jpeg";
+function compress(input, webp, grayscale, quality, originSize, maxWidth) {
+    const format = webp ? "webp" : "jpeg";
 
-	return sharp(input)
-		.grayscale(grayscale)
-		.toFormat(format, {
-			quality: quality,
-			progressive: true,
-			optimizeScans: true
-		})
-		.toBuffer({resolveWithObject: true})
-		.then(({data: output,info}) => {	// this way we can also get the info about output image, like height, width
-		// .toBuffer()
-		// .then( output => {
-			return {
-				err: null,
-				headers: {
-					"content-type": `image/${format}`,
-					"content-length": info.size,
-					"x-original-size": originSize,
-					"x-bytes-saved": originSize - info.size,
-				},
-				output: output
-			};
-		}).catch(err => {
-			return {
-				err: err
-			};
-		});
+    // Kompres dan resize gambar, atur lebar maksimum menjadi 200px
+    return sharp(input)
+        .resize({ width: maxWidth })  // Resize gambar dengan lebar maksimum 200px, menjaga aspek rasio
+        .grayscale(grayscale)
+        .toFormat(format, {
+            quality: quality,  // Kualitas kompresi yang diterima dari `index.js`
+            progressive: true,
+            optimizeScans: true
+        })
+        .toBuffer({ resolveWithObject: true })
+        .then(({ data: output, info }) => {
+            return {
+                err: null,
+                headers: {
+                    "content-type": `image/${format}`,
+                    "content-length": info.size,
+                    "x-original-size": originSize,
+                    "x-bytes-saved": originSize - info.size,
+                },
+                output: output
+            };
+        }).catch(err => {
+            return {
+                err: err
+            };
+        });
 }
 
 module.exports = compress;
