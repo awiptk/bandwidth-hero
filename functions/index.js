d73cea8e771e397bef8b4343a1b9936cd15b81e1
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-10-23 18:04:18 +0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const shouldCompress = require("../util/shouldCompress");
 const compress = require("../util/compress");
 
-const DEFAULT_QUALITY = 40;
+const DEFAULT_QUALITY = 1;
 
 exports.handler = async (event, context) => {
     let { url } = event.queryStringParameters;
