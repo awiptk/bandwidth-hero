80818d437681128124fa15abf0461a4530ba083a
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-10-23 19:34:46 +0700

    Update compress.js

--- util/compress.js
+++ util/compress.js
@@ -1,31 +1,45 @@
 const sharp = require("sharp");
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 
 function compress(input, webp, grayscale, quality, originSize, maxWidth) {
     const format = webp ? "webp" : "jpeg";
 
     return sharp(input)
-        .resize({ width: maxWidth })  // Resize gambar
+        .resize({ width: maxWidth, kernel: sharp.kernel.lanczos3 })  // Gunakan filter Lanczos3 untuk hasil resize halus
         .grayscale(grayscale)
         .toFormat(format, {
-            quality: quality,
-            progressive: true,
+            quality: quality,  // Kualitas kompresi yang lebih tinggi
+            progressive: false,  // Nonaktifkan progressive untuk kualitas maksimum
             optimizeScans: true
         })
         .toBuffer({ resolveWithObject: true })
-        .then(({ data: output, info }) => {
+        .then(async ({ data: output, info }) => {
+            // Jika format adalah JPEG, kita lakukan kompresi tambahan menggunakan Imagemin
+            if (format === "jpeg") {
+                output = await imagemin.buffer(output, {
+                    plugins: [
+                        imageminMozjpeg({ quality: quality })  // Gunakan Imagemin untuk kompresi JPEG yang lebih baik
+                    ]
+                });
+            }
+
             return {
                 err: null,
                 headers: {
                     "content-type": `image/${format}`,
-                    "content-length": info.size,
+                    "content-length": output.length,
                     "x-original-size": originSize,
-                    "x-bytes-saved": originSize - info.size,
+                    "x-bytes-saved": originSize - output.length,
                 },
                 output: output
             };
-        }).catch(err => {
+        })
+        .catch(err => {
             return {
                 err: err
             };
         });
 }
+
+module.exports = compress;
