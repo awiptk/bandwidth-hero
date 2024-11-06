3a4db8b1490bb289a804fa8dc998b4a12b842347
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-11-06 13:28:26 +0700

    Update shouldCompress.js

--- util/shouldCompress.js
+++ util/shouldCompress.js
@@ -1,19 +1,18 @@
 const MIN_COMPRESS_LENGTH = 1024;
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
 
-function shouldCompress(originType, originSize) {
-    // Jika bukan tipe gambar atau ukuran file 0, tidak perlu dikompres
-    if (!originType.startsWith("image") || originSize === 0) return false;
+function shouldCompress(originType, originSize, webp) {
+	if ( !originType.startsWith("image") || originSize === 0) return false;
+	if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
+	if (	// if png or gif image, and size is less than compression limit (also shouldn't be webp)
+		!webp &&
+		(originType.endsWith("png") || originType.endsWith("gif")) &&
+		originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
+	) {
+		return false;
+	}
 
-    // Tidak kompresi jika ukuran lebih kecil dari batas minimal
-    if (originSize < MIN_COMPRESS_LENGTH) return false;
-
-    // Jika gambar PNG atau GIF dan ukurannya lebih kecil dari batas kompresi, tidak perlu dikompres
-    if ((originType.endsWith("png") || originType.endsWith("gif")) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
-        return false;
-    }
-
-    return true;
+	return true;
 }
 
 module.exports = shouldCompress;
