6ca83c5a855096491927290d6034c238b5b40c04
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-11-06 13:56:44 +0700

    Update compress.js

--- util/compress.js
+++ util/compress.js
@@ -1,34 +1,25 @@
 const sharp = require("sharp");
 
-function compress(input, webp, grayscale, quality, originSize) {
-	const format = webp ? "webp" : "jpeg";
+function compress(input, quality, originSize) {
+    const format = "jpeg";  // Ganti ke jpeg, bukan webp
 
-	return sharp(input)
-		.grayscale(grayscale)
-		.toFormat(format, {
-			quality: quality,
-			progressive: true,
-			optimizeScans: true
-		})
-		.toBuffer({resolveWithObject: true})
-		.then(({data: output,info}) => {	// this way we can also get the info about output image, like height, width
-		// .toBuffer()
-		// .then( output => {
-			return {
-				err: null,
-				headers: {
-					"content-type": `image/${format}`,
-					"content-length": info.size,
-					"x-original-size": originSize,
-					"x-bytes-saved": originSize - info.size,
-				},
-				output: output
-			};
-		}).catch(err => {
-			return {
-				err: err
-			};
-		});
+    return sharp(input)
+        .jpeg({ quality, progressive: true, optimizeScans: true })  // Hanya JPEG
+        .toBuffer({ resolveWithObject: true })
+        .then(({ data: output, info }) => {
+            return {
+                err: null,
+                headers: {
+                    "content-type": `image/${format}`,
+                    "content-length": info.size,
+                    "x-original-size": originSize,
+                    "x-bytes-saved": originSize - info.size,
+                },
+                output: output
+            };
+        }).catch(err => {
+            return { err: err };
+        });
 }
 
 module.exports = compress;
